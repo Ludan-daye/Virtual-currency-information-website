dfc1fd4b488f39cd432ad3518f6b197715aeec07
Ludan-daye <Ludan-daye@users.noreply.github.com>
2025-10-19 22:49:38 +0800

    Refine assets and cleanup

--- backend_node_backup/dist/utils/cache.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function cacheGet<T>(key: string): T | undefined;
-export declare function cacheSet<T>(key: string, value: T, ttl?: number): void;
-export declare function cacheWrap<T>(key: string, factory: () => Promise<T>, ttl?: number): Promise<T>;
-//# sourceMappingURL=cache.d.ts.map
--- backend_node_backup/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import coinRoutes from "./coins";
-
-const router = Router();
-
-router.use("/api", coinRoutes);
-
-export default router;
